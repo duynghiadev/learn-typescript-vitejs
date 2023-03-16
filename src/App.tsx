4e498b04d1c10b34cd3f35f03636c73e4e49c99e
duynghiadev <duynghia22302@gmail.com>
2023-03-17 05:33:23 +0700

    171. named và default export

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { Footer, Header } from './components/common';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <h1>Vite + React! Duy Nghia Dev</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-      </div>
+    <div>
+      <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
