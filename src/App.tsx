06a33cef96db22093ea6c6bbb40d31074a0a1f69
duynghiadev <duynghia22302@gmail.com>
2023-03-17 08:28:02 +0700

    173. Thực hành Props P1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { Footer, Header } from './components/common';
+import { Student } from './features/labs/Student';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Student age={18} isHero hobbyList={['eat', 'code', 'sleep']} sayHello={() => {}} />
       <Footer></Footer>
     </div>
   );
