54096870570db1b5ba637b581355fab3027d587f
duynghiadev <duynghia22302@gmail.com>
2023-03-18 04:04:04 +0700

    184. List rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import './App.css';
 import { Footer, Header, Widget } from './components/common';
 import { MainLayout } from './components/Layout';
@@ -28,9 +28,36 @@ function App() {
 
   // if (loading) return <p>Loading...</p>; // case IF operator
 
+  // danh sách bên server thì phải có id
+  const studentList = [
+    { id: 1, name: 'easy' },
+    { id: 2, name: 'front end' },
+  ];
+
+  // danh sách bên client thì không có id. Danh sách này là danh sách cố định
+  const statusList = ['pending', 'active'];
+
   return (
     <div>
-      {/* {loading && <p>loading...</p>} // case AND  operator */}
+      <ul>
+        {studentList.map((student) => (
+          // khi sử dụng List mà dùng hàm map() thì nên thêm key vào
+          // lưu ý key là unit (không được trùng với key khác)
+          <Fragment key={student.id}>
+            <li>{student.name}</li>
+            <li>{student.name}</li>
+          </Fragment>
+        ))}
+      </ul>
+
+      <ul>
+        {statusList.map((status, idx) => (
+          // danh sách cố định nên sử dụng key = index thì OK
+          // còn nếu danh sách đó mà thêm, xoá, sửa thì không nên dử dụng key = index
+          <li key={idx}>{status}</li>
+        ))}
+      </ul>
+      {/* {loading && <p>loading...</p>} // case AND  operator
       {loading ? <p>loading...</p> : 'data ready!'} // case ternary operator
       <MainLayout>
         <StudentCard student={john} onClick={handleStudentClick} />
@@ -68,7 +95,7 @@ function App() {
         <div>
           <Widget title="Earning Overview">chart 4</Widget>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
