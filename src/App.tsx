73a0f5353402744d4accaf9502c092cdccc6647b
duynghiadev <duynghia22302@gmail.com>
2023-03-18 05:15:59 +0700

    189. setState is async

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let i = 0;
+
   const [showMore, setShowMore] = useState(false);
   const [count, setCount] = useState(0);
   const [name, setName] = useState('duynghiadev');
 
   function handleIncreaseClick() {
-    setCount(count + 1);
+    const newCount = count + 1;
+    setCount(count + 1); // những hàm setState là async function
+    console.log('after setting count is old value: ', count); // old value
+    console.log('after setting count is new value: ', newCount); // new value
+
+    i += 5;
   }
+
+  console.log({ count, i });
 
   return (
     <div>
